a77ac3423027665fcd5aa110433b669a5f6f0365
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-10 20:17:17 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Hero from "@/components/Home/Hero";
 import React from "react";
 
 type Props = {};
 
 function page({}: Props) {
-  return <div>page</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default page;
