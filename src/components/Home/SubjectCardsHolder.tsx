0250ab4ff58002fe3bb82bda84a4d437dfe9b7c6
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-11 13:45:28 +0300

    the subject card holder + api call

---
+++ src/components/Home/SubjectCardsHolder.tsx
@@ -0,0 +1,27 @@
+import { Subject } from "@/Types";
+import axios from "axios";
+import SubjectCard from "../SubjectCard/SubjectCard";
+import styles from "./home.module.scss";
+
+async function SubjectCardsHolder() {
+  let subjects: Subject[] = [];
+  try {
+    const response = await axios.get("http://localhost:3000/api/subject");
+    subjects = response.data;
+  } catch (error) {
+    console.error("Error fetching subjects:", error);
+  }
+
+  return (
+    <div className={styles.subjectCardsHolder}>
+      <h1>Our Main Subjects</h1>
+      {subjects.length > 0 ? (
+        subjects.map((subject) => <SubjectCard subject={subject}></SubjectCard>)
+      ) : (
+        <div>No subjects available</div>
+      )}
+    </div>
+  );
+}
+
+export default SubjectCardsHolder;
