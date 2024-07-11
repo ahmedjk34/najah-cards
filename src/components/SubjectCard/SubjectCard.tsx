4324f118934b67db6296e6661026fc05083f6c2c
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-11 13:45:12 +0300

    done with main page subject card

---
+++ src/components/SubjectCard/SubjectCard.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import styles from "./subject-card.module.scss";
+import { Subject } from "@/Types";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+type Props = {
+  subject: Subject;
+};
+const SubjectCard = ({ subject }: Props) => {
+  const router = useRouter();
+  return (
+    <motion.div
+      className={styles.card}
+      initial={{ opacity: 0, x: -15 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: 0.3, ease: "easeInOut" }}
+      onClick={() => router.push(`/subject/${subject._id}`)}
+    >
+      <img src={subject.image}></img>
+      <div>
+        <h2>{subject.title}</h2>
+        <p>{subject.description}</p>
+      </div>
+      <div></div>
+    </motion.div>
+  );
+};
+
+export default SubjectCard;
