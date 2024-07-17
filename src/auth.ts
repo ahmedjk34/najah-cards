76ddcc95e1503565b573491864e57fcff290934b
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-17 17:35:02 +0300

    changed the signIn page

--- src/auth.ts
+++ src/auth.ts
@@ -42,6 +42,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   pages: {
     //here we add those if we don't want to use the default auth.js pages
-    signIn: "/login",
+    signIn: "/auth/login",
   },
 });
