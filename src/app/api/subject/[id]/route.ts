3e536286d65b282e9b4bb8157b630932a0fa4902
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-07-22 21:11:53 +0300

    added page logic

--- src/app/api/subject/[id]/route.ts
+++ src/app/api/subject/[id]/route.ts
@@ -1,5 +1,6 @@
 import connectDB from "@/lib/connectDB";
-import subjectModal from "@/models/subjectModel";
+import deckModel from "@/models/deckModel";
+import subjectModel from "@/models/subjectModel";
 import { MongooseError } from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -8,14 +9,30 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   try {
+    const searchParams = request.nextUrl.searchParams;
+    let page = parseInt(searchParams.get("page") ?? "1");
+
+    //! handle if number of pages is more than the said amount , and less than 1
     await connectDB();
-    const subject = await subjectModal.findById(params.id);
+    const subject = await subjectModel.findById(params.id);
     if (!subject) {
       return new NextResponse("Subject not found", { status: 404 });
     }
-    return NextResponse.json(subject);
+    const numberOfPages = Math.floor(subject.decks.length / 10);
+    if (page < 1) page = 1;
+    else if (page > numberOfPages) page = 1;
+    const decks = await deckModel
+      .find({ _id: { $in: subject.decks } })
+      .populate("author")
+      .skip((page - 1) * 10)
+      .limit(10);
+    subject.decks = decks;
+
+    //we also return the number of pages to handle the pages logic
+    return NextResponse.json({ subject, numberOfPages });
   } catch (error) {
     const e = error as MongooseError;
+    console.log(e);
     return new NextResponse(`ObjectID is invalid or Database is down`, {
       status: 500,
     });
